09b4967500335c32ca80180337b73a0d871baf7d
tnicacio <nicacio.t92@gmail.com>
2021-07-03 17:17:33 -0300

    Custom Notifications for current User

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dslearnbds.entities;
 
+import java.io.Serializable;
 import java.time.Instant;
 
 import javax.persistence.Column;
@@ -13,7 +14,8 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_notification")
-public class Notification {
+public class Notification implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
